df81dfb80430d25ed76d53a67ac7e22f43a899da
ErickVonDircksen <83781289+ErickVonDircksen@users.noreply.github.com>
2022-09-21 16:02:51 -0300

    Update Bloom.py

--- Bloom.py
+++ Bloom.py
@@ -7,34 +7,30 @@ import cv2
 # Alunos Erick H. Dircksen e André L. Gomes
 
 
-INPUT_IMAGE =  'b01 - Original.bmp'
-K_SIZE = 5
-ITER = 5
+INPUT_IMAGE =  'GT2.bmp'
+K_SIZE = 3
+ITER = 4
+SIGMA = 4
+ALPHA = 0.9
 
 def bloomG(img):
     aux = 1
+    img_out= img.copy()
     for aux in range(ITER):
-        img+=cv2.GaussianBlur(img,(K_SIZE,K_SIZE),aux)
+        img_out+=cv2.GaussianBlur(img_out,(K_SIZE,K_SIZE),SIGMA + aux)
         aux *=2 
-    return img    
-
+    return img_out    
 
 def bloomBox(img):
+    img_out= img.copy()
     for aux in range(ITER):
-        img += cv2.blur(img,(K_SIZE,K_SIZE))   
+        img_out += cv2.blur(img_out,(K_SIZE,K_SIZE))   
         aux+=1 
-    return img    
+    return img_out    
 
-def blend(img1,img2):
-    height = img1.shape[0] 
-    length = img1.shape[1] 
-    
-    for y in range(height):
-            for x in range(length):
-                if(img2[x][y] != 0).all():
-                    img1[x][y]+= img2[x][y]
-
-    return img1  
+def blend(img,img2):
+   
+    return (img*ALPHA)+(img2*(1 - ALPHA))
 
 def main ():
 
@@ -42,31 +38,31 @@ def main ():
     if img is None:
         print ('Erro abrindo a imagem.\n')
         sys.exit ()
-    
+
     img = img.reshape ((img.shape [0], img.shape [1], 3))
+    
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # cria a escala de cinza que vai servir pra encontrar as fontes de luz
+    
+    img = img.astype (np.float32) / 255    
+    
+    (T, thresh) = cv2.threshold(gray, 0, 255,   #cria a mascara de luz, usando um thresholding automático provido pelo  cv2.THRESH_OTSU
+	cv2.THRESH_BINARY | cv2.THRESH_OTSU)        # threshold automático foi utilizado pra evitar ter que mudar manualmente para cada imagem.
+    print("[INFO] otsu's thresholding value: {}".format(T)) # informa o threshold utilizado
     
     start_time = timeit.default_timer ()
    
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-   
-    
-    (T, thresh) = cv2.threshold(gray, 0, 255,
-	cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-    print("[INFO] otsu's thresholding value: {}".format(T))
-  
-    img2 = cv2.bitwise_and(img,img,mask=thresh)
-    
-    final2 = blend(img,img2)
-   
-    final_img = bloomG(img) 
-    
+    img2 = cv2.bitwise_and(img,img,mask=thresh) 
 
+    blurred_mask_Box = bloomBox(img2) 
+    
+    blurred_mask = bloomG(img2) 
 
+    final_image_Box = blend(img,blurred_mask_Box)
+    final_image_G = blend(img,blurred_mask)
+    
     print ('Tempo: %f' % (timeit.default_timer () - start_time))
-
-    
-    cv2.imwrite ('01 - output.png', final2)
-
+    cv2.imwrite ('Box-output.png', final_image_Box * 255)
+    cv2.imwrite ('Gaussian-output.png', final_image_G * 255)
 
 if __name__ == '__main__':
     main ()
